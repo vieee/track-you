96ee0e2228a77ec1f8b4e12a4b3bedf8f0c77293
Deepak <dgyadv1782000@gmail.com>
2021-05-29 08:14:30 +0530

    Google Cloud Integration Complete

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 const video = document.getElementById("video");
 let predictedAges = [];
+let loginStatus;
+const ssID = "GOOGLE_SPREAD_SHEET_ID";
+const attendance = {
+  Deepak: "4",
+  Ghanshyam: "5",
+  Sarthak: "6",
+  Krish: "7",
+};
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-  faceapi.nets.ageGenderNet.loadFromUri("/models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models")
+  faceapi.nets.tinyFaceDetector.loadFromUri(
+    "https://raw.githubusercontent.com/vieee/track-you/main/models"
+  ),
+  faceapi.nets.faceLandmark68Net.loadFromUri(
+    "https://raw.githubusercontent.com/vieee/track-you/main/models"
+  ),
+  faceapi.nets.faceRecognitionNet.loadFromUri(
+    "https://raw.githubusercontent.com/vieee/track-you/main/models"
+  ),
+  faceapi.nets.faceExpressionNet.loadFromUri(
+    "https://raw.githubusercontent.com/vieee/track-you/main/models"
+  ),
+  faceapi.nets.ageGenderNet.loadFromUri(
+    "https://raw.githubusercontent.com/vieee/track-you/main/models"
+  ),
+  faceapi.nets.ssdMobilenetv1.loadFromUri(
+    "https://raw.githubusercontent.com/vieee/track-you/main/models"
+  ),
 ]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
   );
 }
 
@@ -25,7 +45,7 @@ video.addEventListener("playing", async () => {
   faceapi.matchDimensions(canvas, displaySize);
 
   const labeledFaceDescriptors = await loadLabelledImages();
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
 
   setInterval(async () => {
     const detections = await faceapi
@@ -36,7 +56,9 @@ video.addEventListener("playing", async () => {
       .withAgeAndGender();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     // console.log(resizedDetections)
-    const results = resizedDetections.map(rd => faceMatcher.findBestMatch(rd.descriptor))
+    const results = resizedDetections.map((rd) =>
+      faceMatcher.findBestMatch(rd.descriptor)
+    );
 
     // console.log(results)
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
@@ -49,7 +71,7 @@ video.addEventListener("playing", async () => {
     const interpolatedAge = interpolateAgePredictions(age);
     const bottomRight = {
       x: resizedDetections[0].detection.box.bottomRight.x - 55,
-      y: resizedDetections[0].detection.box.bottomRight.y
+      y: resizedDetections[0].detection.box.bottomRight.y,
     };
 
     new faceapi.draw.DrawTextField(
@@ -59,15 +81,13 @@ video.addEventListener("playing", async () => {
 
     const topRight = {
       x: resizedDetections[0].detection.box.topRight.x - 45,
-      y: resizedDetections[0].detection.box.topRight.y - 20
+      y: resizedDetections[0].detection.box.topRight.y - 20,
     };
 
-    results.forEach(result => {
-      new faceapi.draw.DrawTextField(
-        [result.toString()],
-        topRight
-      ).draw(canvas);
-      console.log(result)
+    results.forEach((result) => {
+      new faceapi.draw.DrawTextField([result._label], topRight).draw(canvas);
+      // console.log(result);
+      updateDataFromSheet(result._label);
     });
   }, 100);
 });
@@ -80,22 +100,101 @@ function interpolateAgePredictions(age) {
 }
 
 function loadLabelledImages() {
-  const labels = ['Deepak', 'Ghanshyam', 'Sushila', 'Krish', 'Sarthak'];
+  const labels = ["Deepak", "Ghanshyam", "Sushila", "Krish", "Sarthak"];
 
   return Promise.all(
-    labels.map(async label => {
-      const descriptions = []
+    labels.map(async (label) => {
+      const descriptions = [];
       for (let index = 1; index <= 2; index++) {
-        const image = await faceapi.fetchImage(`https://raw.githubusercontent.com/vieee/track-you/main/labelled_images/${label}/${index}.jpeg`);
+        const image = await faceapi.fetchImage(
+          `https://raw.githubusercontent.com/vieee/track-you/main/labelled_images/${label}/${index}.jpeg`
+        );
         const detections = await faceapi
           .detectSingleFace(image)
           .withFaceLandmarks()
           .withFaceDescriptor();
 
-        descriptions.push(detections.descriptor)
+        descriptions.push(detections.descriptor);
       }
 
-      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
     })
-  )
+  );
+}
+
+function handleClientLoad() {
+  gapi.load("client:auth2", initClient);
+}
+
+function initClient() {
+  var API_KEY = "MY_GOOGLE_API_KEY"; // TODO: Update placeholder with desired API key.
+
+  var CLIENT_ID =
+    "MY_GOOGLE_CLIENT_ID"; // TODO: Update placeholder with desired client ID.
+
+  // TODO: Authorize using one of the following scopes:
+  //   'https://www.googleapis.com/auth/drive'
+  //   'https://www.googleapis.com/auth/drive.file'
+  //   'https://www.googleapis.com/auth/drive.readonly'
+  //   'https://www.googleapis.com/auth/spreadsheets'
+  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+  var SCOPE = "https://www.googleapis.com/auth/spreadsheets";
+
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      scope: SCOPE,
+      discoveryDocs: [
+        "https://sheets.googleapis.com/$discovery/rest?version=v4",
+      ],
+    })
+    .then(function () {
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+      updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    });
+}
+
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    console.log("Logged In");
+  } else {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+}
+
+function updateDataFromSheet(name) {
+  var params = {
+    // The ID of the spreadsheet to update.
+    spreadsheetId: ssID, // TODO: Update placeholder value.
+
+    // The A1 notation of the values to update.
+    range: "D" + attendance[name], // TODO: Update placeholder value.
+
+    // How the input data should be interpreted.
+    valueInputOption: "RAW", // TODO: Update placeholder value.
+  };
+
+  var valueRangeBody = {
+    values: [["P"]],
+  };
+
+  var request = gapi.client.sheets.spreadsheets.values.update(
+    params,
+    valueRangeBody
+  );
+  request.then(
+    function (response) {
+      // TODO: Change code below to process the `response` object:
+      // console.log(response.result);
+      JSalert();
+    },
+    function (reason) {
+      console.error("error: " + reason.result.error.message);
+    }
+  );
+}
+
+function JSalert() {
+  swal("Congrats!", ", Your attendance is marked!", "success");
 }
